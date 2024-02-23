646adc49c183536fcee792082c8a1733a298faef
Jared Davidson (Archetapp) <archetapp@gmail.com>
2024-02-23 10:41:37 -0700

    Initial Commit

---
+++ VisualTodo/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  VisualTodo
+//
+//  Created by Jared Davidson on 2/23/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ VisualTodo/VisualTodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisualTodoApp.swift
+//  VisualTodo
+//
+//  Created by Jared Davidson on 2/23/24.
+//
+
+import SwiftUI
+
+@main
+struct VisualTodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
